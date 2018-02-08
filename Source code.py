4c5b160e5e966cbdce088253db41c374b5ae4f85
Tyrell1052 <tyrell1052@gmail.com>
2018-02-07 21:57:44 -0500

    added code for spaceship

--- Source code.py
+++ Source code.py
@@ -1,9 +1,8 @@
-# pygame template - skeleton for a new pygame project
+# Space Invaders
 import pygame
 import random
-import os
 
-WIDTH = 800
+WIDTH = 580
 HEIGHT = 600
 FPS = 60
 
@@ -14,41 +13,39 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# set up assets folders
-game_folder = os.path.dirname('C:\\Users\\tyrel\\Documents\\img')
-img_folder = os.path.join(game_folder, "img")
-
-class player(pygame.sprite.Sprite):
-    # sprite for the player
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.y_speed = 5
-
-    def update(self):
-        self.rect.x += 5
-        self.rect.y  += self.y_speed
-        if self.rect.bottom > HEIGHT -200:
-            self.y_speed = -5
-        if self.rect.top < 200:
-            self.y_speed = 5
-
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
-all_sprites = pygame.sprite.Group()
-player = player()
-all_sprites.add(player)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
 
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game loop
 running = True
 while running:
@@ -64,7 +61,7 @@ while running:
     all_sprites.update()
 
     # Draw / render
-    screen.fill(BLUE)
+    screen.fill(BLACK)
     all_sprites.draw(screen)
     # After drawing everything, flip the display
     pygame.display.flip()
